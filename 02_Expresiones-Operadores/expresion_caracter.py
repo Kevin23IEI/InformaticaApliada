b6ded29d6fd44449fa9542b5a61e60200a67d218
Kevin Escobedo <iei.kevinescobedo@gmail.com>
2023-03-25 19:20:00 -0600

    Update

--- 02_Expresiones-Operadores/expresion_caracter.py
+++ 02_Expresiones-Operadores/expresion_caracter.py
@@ -1,6 +1,8 @@
 import re
-frase = "juanito@hotmail.com.mx"
+frase = "juanito@hotmail9.com.mx"
 patron = '[0-9]+' #Esta es una expresión regular
 exp_final = re.findall(patron, frase)
 print(exp_final)
 print(type(exp_final))
+
+
--- 03_Cadenas/ejemplo_3.py
+++ 03_Cadenas/ejemplo_3.py
@@ -1,2 +1,16 @@
+fact='Esto es una cadena'
+print('fact')
+print(fact + ' esto es una cadena')
+
+fact_two=fact + ' esto es una cadena'
+print(fact_two)
+
+
 a='Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.'.replace('Celsius', 'C')
 print(a)
+
+frase='The "near side" is the part of the Moon that faces the Earth'
+print(frase)
+
+frase="""We only see about 60% of the Moon's surface, this is known as the "near side"."""
+print(frase)
