b75eb56b9e2bc916adb8c630b5e1935a4a06018a
Kevin Escobedo <iei.kevinescobedo@gmail.com>
2023-04-25 20:57:14 -0600

    T_Ejercicios_resueltos

--- 04_Listas/ejercicio_4.py
+++ 04_Listas/ejercicio_4.py
@@ -32,4 +32,70 @@
     back_end = ['Node','Express', 'MongoDB']
 '''
 
+#Ejercicios resueltos
+#1-1. Declare an empty list
+print("##-- Ejercicio 1 --##")
+listaVacia=[ ]
+print(listaVacia)
 
+#2-3. Find the length of your list
+print("##-- Ejercicio 2 --##")
+lista1=['Facebook', 'Google', 'Microsoft', 'manzana', 'IBM', 'Oracle', 'Amazon']
+print(lista1)
+tamaño1=len(lista1)
+print("La lista contiene:", tamaño1, "elementos ")
+
+#3-6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, manzana, IBM, Oracle and Amazon.
+print("##-- Ejercicio 3 --##")
+companies=['Facebook', 'Google', 'Microsoft', 'manzana', 'IBM', 'Oracle', 'Amazon']
+print("La lista de compañias es la siguiente:\n", companies)
+
+#4-9. Print the first, middle and last company
+print("##-- Ejercicio 4 --##")
+tam_companias=len(companies)
+#print(tam_companias)
+medio=int((tam_companias-1)/2) #nos da la el # del elemento medio de la lista
+#print(medio)
+print("1er Compañia: ",companies[0], ", 2da Compañia: ",companies[medio], ", 3er Compañia: ",companies[tam_companias-1])
+
+#5-12. Insert an IT company in the middle of the companies list
+print("##-- Ejercicio 5 --##")
+companies[medio]='Intel'
+print(companies)
+
+
+#6-15. Check if a certain company exists in the it_companies list.
+print("##-- Ejercicio 6 --##")
+#a=input ()
+#existe=a in companies
+existe="Huawei" in companies
+print(existe)
+if existe==True:
+    print("La compañia si existe en la lista")
+else:
+    print("La compañia no existe en la lista")
+
+#7-18. Slice out the first 3 companies from the list
+print("##-- Ejercicio 7 --##")
+corte3=companies[3:]
+print(corte3)
+
+#8-21. Remove the first IT company from the list
+print("##-- Ejercicio 8 --##")
+companies.pop(0)
+print(companies)
+
+
+#9-24. Remove all IT companies from the list
+print("##-- Ejercicio 9 --##")
+del companies[:]
+print (companies)
+
+
+#10-26. Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+print("##-- Ejercicio 10 --##")
+listaunida=front_end+back_end
+print("Las listas unidas es:\n", listaunida)
